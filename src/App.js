2bbd52e224882c0fc4cc40f63952bf7c9784c208
Ariyan Nath <nathariyan97@gmail.com>
2026-04-26 02:46:30 +0530

    today's update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'; {/* this is an important lib that is use for changing the states */}
+import { use, useState } from 'react'; {/* this is an important lib that is use for changing the states */}
 
 function TableCount(){
   return (
@@ -13,9 +13,14 @@ function TableCount(){
 }
 
 function Qualtity(){
+  const [quantity, setQuantity] = useState(0);
   return (
     <div>
-      <p>The qualtity will be here</p>
+      <div className='quantity'>
+        <button className='incre-decre' onClick={() => setQuantity(quantity > 0 ? quantity - 1 : 0)}>-</button>
+        <span>{quantity}</span>
+        <button className='incre-decre' onClick={() => setQuantity(quantity < 50 ? quantity + 1: 50)}>+</button>
+      </div>
     </div>
   );
 }
@@ -34,29 +39,29 @@ function Indian(){ {/* function for indian food */}
       <div className='sub-div'>
         <div className='item'>
           <img className='item_image' src='/images/indian/1.png' alt='Biryani' />
-          <h3>Biryani</h3>
-          <h3>₹ 90</h3>
+          <h3 className='info'>Biryani</h3>
+          <h3 className='info'>₹ 90</h3>
           <Qualtity />
           <Select />
         </div>
         <div className='item'>
           <img className='item_image' src='/images/indian/2.png' alt='Butter Chicken' />
-          <h3>Butter Chicken</h3>
-          <h3>₹ 120</h3>
+          <h3 className='info'>Butter Chicken</h3>
+          <h3 className='info'>₹ 120</h3>
           <Qualtity />
           <Select />
         </div>
         <div className='item'>
           <img className='item_image' src='images/indian/3.png' alt='Masala Dosa' />
-          <h3>Masala Dosa</h3>
-          <h3>₹ 30</h3>
+          <h3 className='info'>Masala Dosa</h3>
+          <h3 className='info'>₹ 30</h3>
           <Qualtity />
           <Select />
         </div>
         <div className='item'>
           <img className='item_image' src='/images/indian/4.png' alt='Samosa' />
-          <h3>Samosa</h3>
-          <h3>₹ 15</h3>
+          <h3 className='info'>Samosa</h3>
+          <h3 className='info'>₹ 15</h3>
           <Qualtity />
           <Select />
         </div>
@@ -68,7 +73,36 @@ function Indian(){ {/* function for indian food */}
 function Chinese(){ {/* function for chinese food */}
   return (
     <div>
-      <p>These are Chinese food!</p>
+      <div className='sub-div'>
+        <div className='item'>
+          <img className='item_image' src='/images/Chinese/1.png' alt='Biryani' />
+          <h3 className='info'>Dim Sum</h3>
+          <h3 className='info'>₹ 70</h3>
+          <Qualtity />
+          <Select />
+        </div>
+        <div className='item'>
+          <img className='item_image' src='/images/Chinese/2.png' alt='Butter Chicken' />
+          <h3 className='info'>Kung Pao Chicken</h3>
+          <h3 className='info'>₹ 90</h3>
+          <Qualtity />
+          <Select />
+        </div>
+        <div className='item'>
+          <img className='item_image' src='images/Chinese/3.png' alt='Masala Dosa' />
+          <h3 className='info'>Mapo Tofu</h3>
+          <h3 className='info'>₹ 80</h3>
+          <Qualtity />
+          <Select />
+        </div>
+        <div className='item'>
+          <img className='item_image' src='/images/Chinese/4.png' alt='Samosa' />
+          <h3 className='info'>Peking Duck</h3>
+          <h3 className='info'>₹ 160</h3>
+          <Qualtity />
+          <Select />
+        </div>
+      </div>
     </div>
   );
 }
@@ -76,7 +110,36 @@ function Chinese(){ {/* function for chinese food */}
 function Korean(){ {/* function for korean food */}
   return (
     <div>
-      <p>These are Korean food!</p>
+      <div className='sub-div'>
+        <div className='item'>
+          <img className='item_image' src='/images/Korean/1.png' alt='Biryani' />
+          <h3 className='info'>Kimchi</h3>
+          <h3 className='info'>₹ 50</h3>
+          <Qualtity />
+          <Select />
+        </div>
+        <div className='item'>
+          <img className='item_image' src='/images/Korean/2.png' alt='Butter Chicken' />
+          <h3 className='info'>Korean Fried Chicken</h3>
+          <h3 className='info'>₹ 90</h3>
+          <Qualtity />
+          <Select />
+        </div>
+        <div className='item'>
+          <img className='item_image' src='images/Korean/3.png' alt='Masala Dosa' />
+          <h3 className='info'>Bibimbap</h3>
+          <h3 className='info'>₹ 130</h3>
+          <Qualtity />
+          <Select />
+        </div>
+        <div className='item'>
+          <img className='item_image' src='/images/Korean/4.png' alt='Samosa' />
+          <h3 className='info'>Bulgogi</h3>
+          <h3 className='info'>₹ 70</h3>
+          <Qualtity />
+          <Select />
+        </div>
+      </div>
     </div>
   );
 }
